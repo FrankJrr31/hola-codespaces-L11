f10ce232b281275b2a90d4862b52bb30906cbc08
FrankJrr31 <134321340+FrankJrr31@users.noreply.github.com>
2023-06-26 19:30:36 +0000

    render

---
+++ src/main/java/com/example/holacodespacesL11/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.holacodespacesL11;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+    
+}
